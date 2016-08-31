14229f5ff316f44187ef12dd43d9efc5ea81ff6a
VaishnaveNavaneethan <ngvaishnave@gmail.com>
2016-08-31 23:25:46 +0530

    Create PangramString.java

---
+++ PangramString.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class PangramString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s=new String();
+		Scanner sc=new Scanner(System.in);
+		s=sc.nextLine();
+		Set<Character> se=new TreeSet<Character>();
+		char[]a=s.toLowerCase().toCharArray();
+		for(int i=0;i<a.length;i++)
+		{
+			if(Character.isAlphabetic(a[i]))
+			{
+			se.add(a[i]);
+			}
+		}
+		System.out.println(se);
+		
+		System.out.println(se.size());
+		if(se.size()==26)
+		{
+			System.out.println("true");
+		}
+		else
+		{
+			System.out.println("false");
+		}
+
+	}
+
+}
