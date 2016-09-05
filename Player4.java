94e549966ebad9e9f4584b47475f7be97ed383bc
VaishnaveNavaneethan <ngvaishnave@gmail.com>
2016-09-05 23:09:39 +0530

    Create Player4.java

---
+++ Player4.java
@@ -0,0 +1,23 @@
+/* package whatever; // don't place package name! */
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Player4
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+		// your code goes here
+		Scanner sc=new Scanner(System.in);
+		String s=new String();
+		s=sc.next();
+		
+		int n=Integer.parseInt(s);
+		
+			System.out.println(n);
+		
+	}
+	
+}
