f55026d802a6b6b13ba06249424d07e87b019e3b
VaishnaveNavaneethan <ngvaishnave@gmail.com>
2016-09-05 21:07:51 +0530

    Create GuviPlayer1.java

---
+++ GuviPlayer1.java
@@ -0,0 +1,14 @@
+package first;
+import java.util.*;
+public class GuviPlayer1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		String str=sc.next();
+		StringBuffer l=new StringBuffer(str);
+		l.reverse();
+		System.out.println(l);
+	}
+
+}
